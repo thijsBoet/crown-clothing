38c27a99caaa6df69c9735b7edf3a18230dbe2bc
Matthijs Boet <m.boet@chello.nl>
2020-04-20 00:38:28 +0200

    created basic routing with React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import './App.css';
+import HomePage from "./pages/homepage/homepage.component.jsx";
 
-import HomePage from './pages/homepage/homepage.component.jsx'
+import "./App.css";
+
+const HatsPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <button onClick={() => props.history.push('/hats')}>Topics</button>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route exact path="/hats" component={ HatsPage } />
+      </Switch>
     </div>
   );
 }
