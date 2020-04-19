ebbe5df5f9905d829a666d3932590f5340ebabad
Matthijs Boet <m.boet@chello.nl>
2020-04-20 01:18:45 +0200

    created shop page, SHOP_DATA, styling/building of collection and collection-preview item components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component"
 
 import "./App.css";
-
-const HatsPage = props => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push('/hats')}>Topics</button>
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ HomePage } />
-        <Route exact path="/hats" component={ HatsPage } />
+        <Route exact path="/shop" component={ ShopPage } />
       </Switch>
     </div>
   );
